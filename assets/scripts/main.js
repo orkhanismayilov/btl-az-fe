25863f855f751e1bb129276ed2015b3685e7177a
orkhanismayilov <orikismayilov@outlook.com>
2024-01-12 15:44:10 +0400

    feat: Add hero section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,13 @@
 `use strict`;
+
+const heroSwiper = document.querySelector('.hero-swiper');
+if (heroSwiper) {
+  new Swiper(heroSwiper, {
+    loop: true,
+    speed: 500,
+    navigation: {
+      enabled: true,
+      nextEl: '.swiper-button-next',
+    },
+  });
+}
