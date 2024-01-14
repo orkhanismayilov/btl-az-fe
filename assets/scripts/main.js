9b078cc81f36178d93f7d80eb7f51cda1dc045dd
orkhanismayilov <orikismayilov@outlook.com>
2024-01-14 18:09:15 +0400

    feat: Add why section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -16,3 +16,25 @@ if (heroSwiper) {
     },
   });
 }
+
+const whySectionCounters = document.querySelectorAll('.why-section-counter-item span:first-child');
+if (whySectionCounters.length) {
+  const { default: counterUp } = window.counterUp;
+  const io = new IntersectionObserver(entries => {
+    for (const entry of entries) {
+      const whyCounter = entry.target;
+      if (entry.isIntersecting) {
+        counterUp(whyCounter, {
+          duration: 1500,
+          delay: 16,
+        });
+
+        io.unobserve(whyCounter);
+      }
+    }
+  });
+
+  for (const whyCounter of whySectionCounters) {
+    io.observe(whyCounter);
+  }
+}
